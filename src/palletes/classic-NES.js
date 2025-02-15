d6eb0d5ddb1f2c3a1897797ef207e02d101bc218
Chris Weed <chris@cjweed.com>
2025-02-15 14:45:54 -0600

    Wipe

--- src/palletes/classic-NES.js
+++
@@ -1,9 +0,0 @@
-import classic from './classic'
-export default [
-  ...classic,
-  ['b82800', 'Raspberry'],
-  ['e75a11', 'Tiger Orange'],
-  ['9c4a00', 'Brown'],
-  ['75ffa5', 'Green'],
-  ['75ffd1', 'Turquoise'],
-]
--- src/palletes/greyscale.js
+++
@@ -1,7 +0,0 @@
-export default [
-  ['ffffff', 'White'],
-  ['848484', 'Light Grey'],
-  ['555555', 'Medium Grey'],
-  ['323232', 'Dark Grey'],
-  ['050505', 'Black'],
-]
